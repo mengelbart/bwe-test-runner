98181611d0160e0c4b01b867bc95d9c753868f48
Mathis Engelbart <mathis.engelbart@gmail.com>
2021-11-23 10:53:59 +0100

    Make sender and receiver configurable via env var

--- endpoint/setup.sh
+++ endpoint/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+set -x
+
 IP=$(hostname -I | cut -f1 -d" ")
 GATEWAY="${IP%.*}.2"
 
 ip route add 172.25.0.0/16 via $GATEWAY
 ip route add 172.26.0.0/16 via $GATEWAY
 ip route add 172.27.0.0/16 via $GATEWAY
-
-/bin/bash
 
